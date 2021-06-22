dafd1e85f4aa71ef480693c7392e4b7f5e7990bf
Surya S <77038659+surya-sd@users.noreply.github.com>
2021-06-22 08:37:45 +0530

    env file added

--- hotstar-clone/src/firebase.js
+++ hotstar-clone/src/firebase.js
@@ -1,11 +1,11 @@
 import firebase from "firebase";
 const firebaseConfig = {
-  apiKey: "AIzaSyD0z6CA5D9f84Bnhj4idJBb7TH7KYGa8Ks",
-  authDomain: "hotstar-clone-d344d.firebaseapp.com",
-  projectId: "hotstar-clone-d344d",
-  storageBucket: "hotstar-clone-d344d.appspot.com",
-  messagingSenderId: "534305774350",
-  appId: "1:534305774350:web:c27350fd385f56732d555f",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROTECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
